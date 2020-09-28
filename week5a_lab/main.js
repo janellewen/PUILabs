761ace492c14ad4edfab61fa77905450b9b88a12
Janelle <janelle.wen@gmail.com>
2020-09-28 13:23:47 -0400

    test

---
+++ week5a_lab/main.js
@@ -0,0 +1,12 @@
+function addNewList(){
+    alert('hello world alert!');
+    console.log('hellow world console');
+}
+
+fuction addListItem() {
+    console.log('hello world')
+    const list = document.getElementById("grocery-list");
+    const itemInput = document.getElementById("new-list-item");
+    const newItem = document.createElement("li");
+}
+
